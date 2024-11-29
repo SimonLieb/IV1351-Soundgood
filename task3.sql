2bcfae29e975b68034b0a519028eaf6be7deaef7
sebtaavo <145364335+sebtaavo@users.noreply.github.com>
2024-11-29 12:34:34 +0100

    task 3 - count lessons for each teacher each month

---
+++ task3.sql
@@ -0,0 +1,26 @@
+SELECT 
+    i.id AS instructor_id,
+    i.name AS instructor_name,
+    COALESCE(l.lesson_count, 0) + COALESCE(e.ensemble_count, 0) AS total_lesson_count
+FROM 
+    instructor i
+LEFT JOIN 
+    (SELECT 
+         instructor_id, 
+         COUNT(*) AS lesson_count 
+     FROM 
+         lesson 
+     WHERE 
+         DATE_TRUNC('month', start_time) = DATE_TRUNC('month', CURRENT_DATE) 
+     GROUP BY 
+         instructor_id) l ON i.id = l.instructor_id
+LEFT JOIN 
+    (SELECT 
+         instructor_id, 
+         COUNT(*) AS ensemble_count 
+     FROM 
+         ensemble 
+     WHERE 
+         DATE_TRUNC('month', start_time) = DATE_TRUNC('month', CURRENT_DATE) 
+     GROUP BY 
+         instructor_id) e ON i.id = e.instructor_id;
